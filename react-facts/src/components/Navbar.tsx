ca800ce9d2b53216a38b31bfac8f2ce0180ac7c7
Sam-Umbra <samuuel2008@hotmail.com>
2025-05-17 21:15:01 -0300

    React Fun Facts - Patch 2

--- react-facts/src/components/Navbar.tsx
+++ react-facts/src/components/Navbar.tsx
@@ -2,9 +2,16 @@ export default function Navbar() {
     return(
     <header>
         <nav>
-            <img src="/src/images/react-logo.png" alt="React Logo"/>
+            <img src="/images/react-logo.png" alt="React Logo"/>
             <span>ReactFacts</span>
         </nav>
     </header>
     )
 }
+
+/* Imagens estáticas não utilizadas em componentes é recomendado deixar no folder public e ao utilzar colocar
+/pasta_da_imagem/imagem
+
+Não há a necessidade de colocar o folder public na url ele le automaticamente
+
+*/
