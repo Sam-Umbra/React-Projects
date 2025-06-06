32a031163a0da72427bfc3512de19e3b3dcbbfe3
Sam-Umbra <samuuel2008@hotmail.com>
2025-06-06 17:06:36 -0300

    Meme Generator - Fase 1

---
+++ meme-generator/src/components/Main.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+type memeTypes = {
+    topText: string,
+    bottomText: string,
+    imageUrl: string
+}
+
+export default function Main() {
+
+    const [meme, setMeme] = useState<memeTypes>({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const { value, name } = event.currentTarget;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }));
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
